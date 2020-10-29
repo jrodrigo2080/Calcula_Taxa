fbaecec00147d7c16f17a4e3be78e258e5ec5b79
jrodrigo2080 <47754746+jrodrigo2080@users.noreply.github.com>
2020-10-29 00:11:05 -0300

    Criação do Projeto
    Subindo o projeto com erro de memoria

---
+++ Calcular Taxa Km/Classes/DAO/DAOConnection.cs
@@ -0,0 +1,36 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calcular_Taxa_Km.Classes.DAO
+{
+    class DAOConnection
+    {
+        
+        DAOUtil util = new DAOUtil();
+        public FbConnection conexao = new FbConnection("User= SYSDBA;Password= masterkey;Database=C:\\MvarandasTecnologia\\Database\\NETUNO.FDB");
+        public void Conectar()
+        {
+            
+            try
+            {
+                if (conexao.State == System.Data.ConnectionState.Closed) 
+                 
+                    conexao.Open();
+                
+            }catch(Exception e)
+            {
+                util.Log("Erro ao conectar ao banco de dados: "+e.Message);
+            }
+
+        }
+
+       
+
+    }
+}
---
+++ Calcular Taxa Km/Classes/DAO/DAOUtil.cs
@@ -0,0 +1,57 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcular_Taxa_Km.Classes.DAO
+{
+    class DAOUtil
+    {
+
+        string sql;
+        DAOConnection conexao = new DAOConnection();
+        FbCommand comando;
+
+        //Metodo Para criar o Log
+        public void Log(string msg)
+        {
+            var caminho = @"c:\MvarandasTecnologia\MenewPdv\logs\taxa\log-" + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            StreamWriter file = new StreamWriter(caminho, true, Encoding.Default);
+            file.WriteLine(DateTime.Now + " > " + msg);
+            file.Dispose();
+        }
+
+        //Metodo para carregar dados na tabela
+        public void CarregarDados(string script, DataGridView tabela)
+        {
+            sql = script;
+            conexao.Conectar();
+            comando = new FbCommand(sql, conexao.conexao);
+            try
+            {
+                FbDataAdapter adapter = new FbDataAdapter(comando);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                tabela.DataSource = dt;
+                Log("Carregando a tabela.....");
+
+            }
+            catch (Exception ex)
+            {
+                Log(ex.Message);
+            }
+        }
+        //metodo para salvar novas taxas
+        public void SalvarTaxa(float ate,float atekm,float valor)
+        {
+
+        }
+
+
+    }
+}
